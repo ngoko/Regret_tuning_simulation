90e83b7311532d134140f2352745698fc6f3e032
ngoko <yanik.ngoko@gmail.com>
2015-10-14 07:58:12 +0200

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -2,7 +2,7 @@ from regret_tuning import RegretTuning
 
 r = RegretTuning(datafile='./satruntime.csv', request_time_slot=700, conf_per_slot=1)
 regret_period = 700*10
-v = r.run_method(self, 'lex', regret_period)
+v = r.run_method('lex', regret_period)
 print v
-v = r.run_method(self, 'rand', regret_period)
+v = r.run_method('rand', regret_period)
 print v
