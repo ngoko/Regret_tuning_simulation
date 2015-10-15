91a9cb30a60bea956bb3e082199b91983d289f92
ngoko <yanik.ngoko@gmail.com>
2015-10-15 07:55:05 +0200

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -3,19 +3,19 @@ from regret_tuning import RegretTuning
 r = RegretTuning(datafile='./satruntime.csv', request_time_slot=600, conf_per_slot=100)
 regret_period = 600*72
 
-n = 100
+n = 150
 u = 0
 v = 0
 w = 0
-
-#for i in range(0, n):
-#  r.shuffle_data()
-#  u +=  max(r.run_method('lex', regret_period))
-#  v +=  max(r.run_method('rand', regret_period))
-#  w +=  max(r.run_method('randsort', regret_period))
-
-#print u/n, v/n, w/n
-
-r.shuffle_data()
-x = r.run_method('randw', regret_period)
-print max(x)
+x = 0
+y = 0
+z = 0
+for i in range(0, n):
+  r.shuffle_data()
+#  z +=  max(r.run_method('randwnoh', regret_period))
+  u +=  max(r.run_method('lex', regret_period))
+  v +=  max(r.run_method('rand', regret_period))
+  w +=  max(r.run_method('randsort', regret_period))
+  x +=  max(r.run_method('randsortdesc', regret_period))
+#  y +=  max(r.run_method('randw', regret_period))
+print u/n, v/n, w/n, x/n, y/n, z/n
